08c6494d3c13fadfcbc1dc0a8c21f5e60a7bdc83
Jared Sim <jared.sim.2014@smu.edu.sg>
2015-11-13 20:02:53 +0800

    Add shell scripts for mac/linux

---
+++ run.sh
@@ -0,0 +1,1 @@
+java -cp classes:lib/commons-codec-1.6.jar:lib/httpcore-4.3.2.jar:lib/commons-logging-1.1.3.jar:lib/httpmime-4.3.5.jar:lib/fluent-hc-4.3.5.jar:lib/jackson-annotations-2.2.3.jar:lib/httpclient-4.3.5.jar:lib/jackson-core-2.2.3.jar:lib/httpclient-cache-4.3.5.jar:lib/jackson-databind-2.2.3.jar JSONChecker $1
